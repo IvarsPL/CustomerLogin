6ab0c3eff585fa88cb057166e63a5d6a58ed0778
IvarsPL <plaudisivars@gmail.com>
2021-10-25 15:24:02 +0300

    Table connection done

---
+++ WebLogin/Data/Migrations/20211025113514_Questions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebLogin.Data.Migrations
+{
+    public partial class Questions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Question",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerQuestion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpecialistName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Question", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Question");
+        }
+    }
+}
---
+++ WebLogin/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLogin.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string CustomerQuestion { get; set; }
+        public string SpecialistName { get; set; }
+
+        public Question()
+        {
+        }
+    }
+}
