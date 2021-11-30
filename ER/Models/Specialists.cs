18cf43e7525aad089fd404ca387f13ac6a4c18e1
IvarsPL <plaudisivars@gmail.com>
2021-11-30 13:11:01 +0200

    restart

---
+++ ER/Models/Specialists.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ER.Models
+{
+    public class Specialists
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string Email { get; set; }
+        public string Profession { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+    }
+}
---
+++ ER/Pages/Master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ER.Pages
+{
+    public class Master
+    {
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string Email { get; set; }
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        //public override string ToString() => JsonSerializer.Serialize<Master>(this);
+    }
+}
