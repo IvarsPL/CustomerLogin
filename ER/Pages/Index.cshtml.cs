02a20d65d52dc9fab655a0a26057bd89c755bdc9
IvarsPL <plaudisivars@gmail.com>
2021-11-30 13:11:01 +0200

    restart

---
+++ ER/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ER.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public Master Master { get; set; }
+
+        public List<string> Pakalpojumi = new List<string>()
+        {
+            "Hair",
+            "Nails",
+            "Massage",
+            "SPA",
+            "Beauticians",
+            "Products"
+        };
+    }
+}
